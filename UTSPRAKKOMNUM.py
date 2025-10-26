852536c5323e085e8975c4e5e782f99b81bda975
Putroe Fatimah Azzahra <azzahra24@mhs.usk.ac.id>
2025-10-26 23:27:25 +0700

    Update UTSPRAKKOMNUM.py

--- UTSPRAKKOMNUM.py
+++ UTSPRAKKOMNUM.py
@@ -44,3 +44,48 @@ class NewtonLayout(BoxLayout):
 
         # Input persamaan f(x)
         input_layout.add_widget(Label(text="Persamaan f(x):", color=(0, 0, 0), font_size=16, halign="right"))
+                self.fx_input = TextInput(
+            text='',
+            multiline=False,
+            size_hint_y=None,
+            height=30,
+            background_color=(0.949, 0.494, 0.820, 1),
+            foreground_color=(0, 0, 0)
+        )
+        input_layout.add_widget(self.fx_input)
+
+        # Label turunan otomatis (tampil langsung setiap kali input berubah)
+        input_layout.add_widget(Label(text="f'(x) (turunan):", color=(0, 0, 0), font_size=16, halign="right"))
+        self.fpx_label = BoxLabel(
+            text="(otomatis muncul saat mengetik)",
+            color=(0, 0, 0),
+            font_size=14,
+            size_hint_y=None,
+            height=30
+        )
+        input_layout.add_widget(self.fpx_label)
+
+        # Input x₀
+        input_layout.add_widget(Label(text="x0:", color=(0, 0, 0), font_size=16, halign="right"))
+        self.x0_input = TextInput(
+            text='',
+            multiline=False,
+            size_hint_y=None,
+            height=30,
+            background_color=(0.949, 0.494, 0.820, 1),
+            foreground_color=(0, 0, 0)
+        )
+        input_layout.add_widget(self.x0_input)
+
+        # Input toleransi ε
+        input_layout.add_widget(Label(text="ε (toleransi):", color=(0, 0, 0), font_size=16, halign="right"))
+        self.e_input = TextInput(
+            text='',
+            multiline=False,
+            size_hint_y=None,
+            height=30,
+            background_color=(0.949, 0.494, 0.820, 1),
+            foreground_color=(0, 0, 0)
+        )
+        input_layout.add_widget(self.e_input)
+
